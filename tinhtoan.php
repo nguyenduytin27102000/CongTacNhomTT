51b968e555bb77ee38d7f25977223ab1c028ee7f
Phuoc <phuocsama009@gmail.com>
2021-11-26 09:58:42 +0700

    Phep nhan

--- tinhtoan.php
+++ tinhtoan.php
@@ -1,37 +1,53 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tính toán</title>
 </head>
-
 <body>
+    
+    <!-- Xử lý dữ liệu post lên -->
     <?php
-    $soA = $soB = 0;
+        $soA = $soB = $ketQua = "";
 
-    if (isset($_POST['soA']) && isset($_POST['soB'])) {
-        $soA=$_POST['soA'];
-        $soB=$_POST['soB'];
-    }
-    function PhepCong($a, $b)
-    {
-        return $a + $b;
-    }
+        $error = array();
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $soA = $_POST['soA'];
+            $soB = $_POST['soB'];
+
+            if (empty($error)) {
+                $ketQua = $soA * $soB;
+            }
+        }
     ?>
 
-    <h1>Form nhap phep tinh</h1>
-    <form action="" method="POST">
-        <label for="soA">Nhap so A:</label>
-        <input type="text" name="soA" id="soA" value="<?php echo $soA ?>">
-        <label for="soB">Nhap so B:</label>
-        <input type="text" name="soB" id="soB" value="<?php echo $soB ?>">
-        <label for="kq">Ket qua: </label>
-        <input type="text" name="kq" id="kq" value="<?php echo PhepCong($soA, $soB) ?>">
-        <input type="submit" value="Tinh">
+    <form action="" method="post">
+        <table>
+            <tr>
+                <td>Nhập a: </td>
+                <td>
+                    <input type="text" name="soA" value="<?php echo $soA; ?>">
+                </td>
+            </tr>
+            <tr>
+                <td>Nhập b: </td>
+                <td>
+                    <input type="text" name="soB" value="<?php echo $soB; ?>">
+                </td>
+            </tr>
+            <tr>
+                <td>Kết quả: </td>
+                <td>
+                    <input type="text" value="<?php echo $ketQua; ?>">
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <input type="submit" value="Tính">
+                </td>
+            </tr>
+        </table>
     </form>
-</body>
 
+</body>
 </html>
